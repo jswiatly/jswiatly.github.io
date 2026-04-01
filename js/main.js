9531bbb94504d9b03b57d96965448637d8d99788
Julian Światły <62251523+jswiatly@users.noreply.github.com>
2026-04-01 20:53:02 +0200

    update style

--- js/main.js
+++ js/main.js
@@ -2,13 +2,14 @@ const INITIAL_MESSAGE =
   "Initializing Connection... Connection established. Welcome to Holiano.dev - Vision in Depth. Type 'help' for available commands.";
 
 const commands = {
-  help: "<br> - <b>whoami</b> <br> - <b>skills</b> <br> - <b>contact</b> <br> - <b>clear</b>",
+  help: "- <b>whoami</b> <br> - <b>skills</b> <br> - <b>contact</b> <br> - <b>clear</b>",
   whoami:
     "> Name: J. Światły <br>> Role: Deep Tech Architect <br>> Status: Diving in code...",
   skills:
     "Available Modules: [A] C, C++, C# [B] Systems Design [C] Embedded Systems & Low-level Development",
   contact:
-    'Mail :<a href="mailto:dev@holiano.dev">dev@holiano.dev</a> <br> GitHub: <a href="https://github.com/jswiatly" target="_blank">@jswiatly</a>',
+    'Mail:<a href="mailto:dev@holiano.dev">dev@holiano.dev</a> <br>' +
+    'GitHub: <a href="https://github.com/jswiatly" target="_blank">@jswiatly</a>',
 };
 
 const DOM = {
